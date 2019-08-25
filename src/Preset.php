7457d4c88d79b13b60f37e7b7c792d95fb7f32c1
Andrew Earls <andrewearls@rocketmail.com>
2019-08-25 16:54:48 -0700

    procfile for heroku

--- src/Preset.php
+++ src/Preset.php
@@ -11,10 +11,16 @@ class Preset extends LaravelPreset
 	public static function install() 
 	{
 		static::updateEnv();
+		static::createProcfile();
 	}
 
 	public static function updateEnv()
 	{
 		copy(__DIR__."/stubs/.env", base_path('.env'));
+	}
+
+	public static function createProcfile()
+	{
+		copy(__DIR__."/stubs/Procfile", base_path('Procfile'));
 	}
 }
